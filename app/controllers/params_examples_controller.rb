2fe4e49a2559dfd99e6ce3791a7152cfe9ce1f83
Dani Zaghian <dani.zaghian@gmail.com>
2018-01-17 19:37:24 -0800

    query params solutions

--- app/controllers/params_examples_controller.rb
+++ app/controllers/params_examples_controller.rb
@@ -6,4 +6,29 @@ class ParamsExamplesController < ApplicationController
     render json: {message: "Your message is #{input_message}", second_message: "Your second message is #{second_message}"}
   end
 
+  def my_name_method
+    input_name = params["query_name"]
+    output_message = "Hey, your name doesn't start with an A!"
+    if input_name.upcase.starts_with?("A")
+      output_message = "Hey, your name starts with an A!"
+    end
+    render json: {
+      name: "#{input_name.upcase}",
+      message: output_message
+    }
+  end
+
+  def guess_game_method
+    winning_number = 77
+    input_guess = params["user_guess"].to_i
+    if input_guess > winning_number
+      output_message = "Pick lower!"
+    elsif input_guess < winning_number
+      output_message = "Pick higher!"
+    else
+      output_message = "You win!"
+    end
+    render json: {guess: input_guess, message: output_message}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  get '/purple_hippo' => 'params_examples#query_params'
+  get '/query_params' => 'params_examples#query_params'
+  get '/my_name' => 'params_examples#my_name_method'
+  get '/guess_game' => 'params_examples#guess_game_method'
+
+  get '/url_segment_params/:wildcard_variable' => 'params_examples#url_segment_params'
 end
